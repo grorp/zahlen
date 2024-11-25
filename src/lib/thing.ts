17d749c88b196f7a79dce63d2ec8a9720f7f00d4
grorp <grorp@posteo.de>
2024-11-25 15:50:28 +0100

    Yay

--- src/lib/thing.ts
+++ src/lib/thing.ts
@@ -28,47 +28,47 @@ const trySolution = (
 	// One Number Is Correct But Wrongly Placed
 	const cond1_count = checkCond(cond1, solution, correctButWronglyPlaced);
 	if (cond1_count != 1) {
-		console.log(
-			`'One Number Is Correct But Wrongly Placed' with ${cond1.join()} failed for ${solution.join()}`,
-		);
+		// console.log(
+		// 	`'One Number Is Correct But Wrongly Placed' with ${cond1.join()} failed for ${solution.join()}`,
+		// );
 		return false;
 	}
 
 	// Two Numbers Are Correct But Wrongly Placed
 	const cond2_count = checkCond(cond2, solution, correctButWronglyPlaced);
 	if (cond2_count != 2) {
-		console.log(
-			`'Two Numbers Are Correct But Wrongly Placed' with ${cond2.join()} failed for ${solution.join()}`,
-		);
+		// console.log(
+		// 	`'Two Numbers Are Correct But Wrongly Placed' with ${cond2.join()} failed for ${solution.join()}`,
+		// );
 		return false;
 	}
 
 	// One Number Is Correct And Well Placed
 	const cond3_count = checkCond(cond3, solution, correctAndWellPlaced);
 	if (cond3_count != 1) {
-		console.log(
-			`'One Number Is Correct And Well Placed' with ${cond3.join()} failed for ${solution.join()}`,
-		);
+		// console.log(
+		// 	`'One Number Is Correct And Well Placed' with ${cond3.join()} failed for ${solution.join()}`,
+		// );
 		return false;
 	}
 
 	// One Number is Correct But Wrongly Placed
 	const cond4_count = checkCond(cond4, solution, correctButWronglyPlaced);
 	if (cond4_count != 1) {
-		console.log(
-			`'One Number Is Correct But Wrongly Placed' with ${cond4.join()} failed for ${solution.join()}`,
-		);
+		// console.log(
+		// 	`'One Number Is Correct But Wrongly Placed' with ${cond4.join()} failed for ${solution.join()}`,
+		// );
 		return false;
 	}
 
 	// Nothing Is Correct
 	const cond5_count = checkCond(cond5, solution, anythingCorrect);
 	if (cond5_count != 0) {
-		console.log(`'Nothing Is Correct' with ${cond5.join()} failed for ${solution.join()}`);
+		// console.log(`'Nothing Is Correct' with ${cond5.join()} failed for ${solution.join()}`);
 		return false;
 	}
 
-	console.log(`${solution.join()} passed`);
+	// console.log(`${solution.join()} passed`);
 	return true;
 };
 
@@ -78,7 +78,9 @@ const findSolutions = (cond1: Thing, cond2: Thing, cond3: Thing, cond4: Thing, c
 	for (let x = 0; x < 10; x++) {
 		for (let y = 0; y < 10; y++) {
 			for (let z = 0; z < 10; z++) {
-				if (trySolution([x, y, z], cond1, cond2, cond3, cond4, cond5)) solutions.push([x, y, z]);
+				if (trySolution([x, y, z], cond1, cond2, cond3, cond4, cond5)) {
+					solutions.push([x, y, z]);
+				}
 			}
 		}
 	}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { findSolutions } from "$lib/thing";
 	import type { Thing } from "$lib/thing";
+	import type { Snapshot } from "./$types";
 	import Condinput from "./condinput.svelte";
 
 	let cond1: Thing = $state([0, 0, 0]);
@@ -12,25 +13,35 @@
 	let solutions: Thing[] = $derived.by(() => {
 		return findSolutions(cond1, cond2, cond3, cond4, cond5);
 	});
+
+	export const snapshot: Snapshot<Thing[]> = {
+		capture: () => [cond1, cond2, cond3, cond4, cond5],
+		restore: (data) => {
+			console.log("restoring from data ", data);
+			[cond1, cond2, cond3, cond4, cond5] = data;
+		},
+	};
 </script>
 
-<div>
-	<h1>Solver</h1>
+<div class="grid h-screen place-items-center bg-slate-100">
+	<div class="flex max-w-screen-sm flex-col gap-8 rounded-xl bg-white p-8 text-slate-700 shadow-lg">
+		<h1 class="text-2xl font-bold text-slate-900">The Thing</h1>
 
-	<Condinput id="cond1" label="One Number Is Correct But Wrongly Placed" bind:value={cond1} />
-	<Condinput id="cond2" label="Two Numbers Are Correct But Wrongly Placed" bind:value={cond2} />
-	<Condinput id="cond3" label="One Number is Correct And Well Placed" bind:value={cond3} />
-	<Condinput id="cond4" label="One Number is Correct But Wrongly Placed" bind:value={cond4} />
-	<Condinput id="cond5" label="Nothing Is Correct" bind:value={cond5} />
+		<Condinput id="cond1" label="One Number Is Correct But Wrongly Placed" bind:value={cond1} />
+		<Condinput id="cond2" label="Two Numbers Are Correct But Wrongly Placed" bind:value={cond2} />
+		<Condinput id="cond3" label="One Number is Correct And Well Placed" bind:value={cond3} />
+		<Condinput id="cond4" label="One Number is Correct But Wrongly Placed" bind:value={cond4} />
+		<Condinput id="cond5" label="Nothing Is Correct" bind:value={cond5} />
 
-	<h3>Solutions</h3>
-	{#if solutions.length > 0}
-		<ul>
-			{#each solutions as sol}
-				<li>{sol.join("")}</li>
-			{/each}
-		</ul>
-	{:else}
-		<p>No solutions found.</p>
-	{/if}
+		<h3 class="text-lg font-bold text-slate-900">Solutions</h3>
+		{#if solutions.length > 0}
+			<ul class="flex flex-wrap gap-4">
+				{#each solutions as sol}
+					<li>{sol.join("")}</li>
+				{/each}
+			</ul>
+		{:else}
+			<p>No solutions found.</p>
+		{/if}
+	</div>
 </div>
